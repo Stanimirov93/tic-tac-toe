0eb0b5299ed3e379faee05f8922daa610bd34621
Angel Stanimirov <angelstanimirov@Angels-Mac.local>
2022-01-25 19:14:51 +0200

    final commit

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,59 @@
 import React, { useState } from "react";
-import Square from "./Square.js";
+import Square from "./Square";
+import "../index.css";
 
 function Board() {
   const [square, setSquare] = useState(Array(9).fill(null));
+  const [X, setX] = useState(true);
+
+  const winner = calculateWinner(square);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Player Turn: " + (X ? "X" : "O");
+  }
 
   const renderSquare = (i) => {
     return <Square value={square[i]} onClick={() => handleClick(i)} />;
   };
 
   const handleClick = (i) => {
-    console.log(i);
+    const squares = square.slice();
+    if (squares[i] === null) {
+      squares[i] = X ? "X" : "O";
+      setSquare(squares);
+      setX(!X);
+    } else {
+      alert("Can't do that");
+    }
   };
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  }
 
   return (
     <div className="board">
@@ -29,6 +72,8 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
+
+      <h3>{status}</h3>
     </div>
   );
 }
